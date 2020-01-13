c2f05bdcd92ba126dd74b633855d5c627538cfaf
Anachron <gith@cron.world>
2020-01-13 19:31:35 +0100

    Dialog: Add events to trigger and continue work on Dialog & DialogButton

--- src/handles/dialog.ts
+++ src/handles/dialog.ts
@@ -2,14 +2,6 @@
 
 import { Handle } from "./handle";
 
-// native DialogCreate                 takes nothing returns dialog
-// native DialogDestroy                takes dialog whichDialog returns nothing
-// native DialogClear                  takes dialog whichDialog returns nothing
-// native DialogSetMessage             takes dialog whichDialog, string messageText returns nothing
-// native DialogAddButton              takes dialog whichDialog, string buttonText, integer hotkey returns button
-// native DialogAddQuitButton          takes dialog whichDialog, boolean doScoreScreen, string buttonText, integer hotkey returns button
-// native DialogDisplay                takes player whichPlayer, dialog whichDialog, boolean flag returns nothing
-
 export class DialogButton extends Handle<button> {
   dialog: Dialog;
 
@@ -20,6 +12,10 @@ export class DialogButton extends Handle<button> {
       super(DialogAddQuitButton, [whichDialog.handle, score, text, hotkey]);
     }
     this.dialog = whichDialog;
+  }
+  
+  public static fromHandle(handle: button): DialogButton {
+    return this.get(handle);
   }
 }
 
