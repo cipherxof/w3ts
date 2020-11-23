e16504c3599df3055fb29047b2532d3c00004ea2
Sam Marquart <marquart.sam@gmail.com>
2020-11-23 11:51:58 -0700

    Add Color and useful color globals

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export * from "./handles/index";
 export * from "./hooks/index";
 export * from "./system/index";
+export * from "./helper/index";
 export { tsGlobals };
 import * as tsGlobals from "./globals/index";
