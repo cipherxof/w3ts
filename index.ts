ab0b12d8163220b3d2d25c409be4275b8e5ec74e
trigger <triggerhappy187@live.com>
2020-11-24 12:05:16 -0800

    rename util -> utils

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export * from "./handles/index";
 export * from "./hooks/index";
 export * from "./system/index";
-export * from "./util/index";
+export * from "./utils/index";
 export { tsGlobals };
 import * as tsGlobals from "./globals/index";
