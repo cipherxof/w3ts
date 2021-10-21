3431f4c38bfc0bfdca654bfe297ac60b22366e5e
Z-Machine <Z-Machine@users.noreply.github.com>
2021-10-21 10:00:46 -0500

    moved in special types from war3-types

---
+++ types/w3ts.d.ts
@@ -0,0 +1,15 @@
+/** @noSelfInFile **/
+
+/**
+ * This macro will inject the contents of the specified file into the source
+ * where it is called. Paths are relative to the project's root.
+ * @example
+ * include("node_modules/resource/file.lua")
+ */
+ declare function include(path: string): void;
+
+ /**
+  * @param any Expression to be evaluated by Node.
+  */
+ declare function compiletime(any: any): any;
+ 
