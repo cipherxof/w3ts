f072b471bf45d0cf492289302d589237dba6df95
trigger <triggerhappy187@live.com>
2021-10-21 22:30:00 -0700

    async sleep function

--- utils/index.ts
+++ utils/index.ts
@@ -1,1 +1,13 @@
+import { Timer } from "../handles/timer";
+
 export * from "./color";
+
+async function sleep(howMuch: number): Promise<null> {
+  return await new Promise((resolve, reject) => {
+    const t = new Timer();
+    t.start(howMuch, false, () => {
+      t.destroy();
+      resolve(null);
+    });
+  });
+}
