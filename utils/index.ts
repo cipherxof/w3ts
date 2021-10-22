66a75975bd260119e358eb4c5f69a47c93f03cad
trigger <triggerhappy187@live.com>
2021-10-21 22:33:13 -0700

    export sleep..

--- utils/index.ts
+++ utils/index.ts
@@ -2,7 +2,7 @@ import { Timer } from "../handles/timer";
 
 export * from "./color";
 
-async function sleep(howMuch: number): Promise<null> {
+export async function sleep(howMuch: number): Promise<null> {
   return await new Promise((resolve, reject) => {
     const t = new Timer();
     t.start(howMuch, false, () => {
